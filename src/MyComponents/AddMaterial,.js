555a41369c6870eccae031130bdbb1ec9fcf8919
Santosh Ghatani <santght90@gmail.com>
2022-02-14 01:30:39 +0545

    addMaterial

--- src/MyComponents/AddMaterial,.js
+++ src/MyComponents/AddMaterial,.js
@@ -1,27 +1,55 @@
 import { useState } from "react";
 import axios from "axios";
 
-const Addmaterial =()=>{
-    const [MaterialName, setMaterialName]= useState('');
-    const [MaterialPrice, setMaterialPrice]= useState('');
-    const [MaterialQuantity, setMaterialQuantity]= useState('');
+const Addmaterial = () => {
+    const [MaterialName, setMaterialName] = useState('');
+    const [MaterialPrice, setMaterialPrice] = useState('');
+    const [MaterialQuantity, setMaterialQuantity] = useState('');
     const [msg, setMsg] = useState('');
-    const addmaterial=(e)=>{
+    const addmaterial = (e) => {
         e.preventDefault();
-        const materialData = {MaterialName, MaterialPrice, MaterialQuantity};
-        const config ={
-            header : {
-                Authorization : 'Bearer' + localStorage.getItem('token')
+        const materialData = { MaterialName, MaterialPrice, MaterialQuantity };
+        const config = {
+            header: {
+                Authorization: 'Bearer' + localStorage.getItem('token')
             }
         }
         axios.post("http://localhost:90/material/add", materialData, config)
-        .then(result=>setMsg(result.data.message))
-        .catch()
+            .then(result => setMsg(result.data.message))
+            .catch()
     }
 
-    return(
-        <div className="container">
-            <div className="row">
+    return (
+        <div className="container items">
+            <div class="form">
+                <div class="title">Welcome</div>
+                <div class="subtitle">Let's add your material!</div>
+                <div class="input-container ic1">
+                    <input id="MaterialName" class="input" type="text" placeholder=" " 
+                    value={MaterialName}
+                    onChange={(e)=>setMaterialName(e.target.value)}/>
+                    <div class="cut"></div>
+                    <label for="MaterialName" class="placeholderM">Material Name</label>
+                </div>
+                <div class="input-container ic2">
+                    <input id="MaterialPrice" class="input" type="number" placeholder=" " 
+                    value={MaterialPrice}
+                    onChange={(e)=>setMaterialPrice(e.target.value)}/>
+                    <div class="cut"></div>
+                    <label for="MaterialPrice" class="placeholderM">Material Price</label>
+                </div>
+                <div class="input-container ic2">
+                    <input id="MaterialQuantity" class="input" type="number" placeholder=" " 
+                    value={MaterialQuantity}
+                    onChange={(e)=>setMaterialQuantity(e.target.value)}/>
+                    <div class="cut cut-short"></div>
+                    <label for="MaterialQuantity" class="placeholderM">Material Quantity</label>
+                </div>
+                <button type="text" class="submit" onClick={addmaterial}>submit</button>
+            </div>
+
+
+            {/* <div className="row">
                 <div className="col-md-4"></div>
                 <div className="col-md-4">
                     <h2 className="heading-h2-all">Add Material</h2>
@@ -48,7 +76,7 @@ const Addmaterial =()=>{
                     </form>
                     {msg}
                 </div>
-            </div>
+            </div> */}
 
         </div>
     )
